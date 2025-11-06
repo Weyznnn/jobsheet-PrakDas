5ffcdbcfd13213382603d5466a9cc56ef73a4399
Weyznnn <rafaninoadiyatma0989@gmail.com>
2025-11-06 16:53:26 +0700

    Percobaan 4 Modifikasi1

--- jobsheet9-array1/searchNilai24.java
+++ jobsheet9-array1/searchNilai24.java
@@ -1,17 +1,31 @@
+import java.util.Scanner;
+
 public class searchNilai24 {
     public static void main(String[] args) {
-        int[] arrNilai = {80, 85, 78, 96, 90, 82, 86};
-        int key = 90;
-        int hasil = 0;
+        Scanner sc = new Scanner(System.in);
+        int key, hasil = 0;
+
+        System.out.print("Masukkan banyaknya bilangan yang akan diinput: ");
+        int jmlElm = sc.nextInt();
+        int[] arrNilai = new int[jmlElm];
+
+        for (int i = 0; i < arrNilai.length; i++) {
+            System.out.print("Masukkan nilai mahasiswa ke-"+(i+1)+": ");
+            arrNilai[i] = sc.nextInt();
+        }
+
+        System.out.print("Masukkan nilai yang ingin dicari: ");
+        key = sc.nextInt();
+        sc.close();
 
         for (int i = 0; i < arrNilai.length; i++) {
             if (key == arrNilai[i]) {
-                hasil = i;
+                hasil = i+1;
                 break;
             }
         }
         System.out.println();
-        System.out.println("Nilai "+key+" ketemu di index ke-"+hasil);
+        System.out.println("Nilai "+key+" ketemu, merupakan nilai mahasiswa ke-"+hasil);
         System.out.println();
     }
 }
