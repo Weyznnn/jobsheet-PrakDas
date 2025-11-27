f3e4513de97fdacaa84458c700a94f7a7f8c5eba
Weyznnn <rafaninoadiyatma0989@gmail.com>
2025-11-27 10:39:33 +0700

    Percobaan 1

---
+++ jobsheet12_fungsi1_pertemuan14/kafe24.java
@@ -0,0 +1,18 @@
+package jobsheet12_fungsi1_pertemuan14;
+
+public class kafe24 {
+    public static void Menu() {
+        System.out.println("===== MENU RESTO KAFE =====");
+        System.out.println("1. Kopi Hitam - Rp 15,000");
+        System.out.println("2. Cappuccino - Rp 20,000");
+        System.out.println("3. Latte - Rp 22,0000");
+        System.out.println("4. Roti Goreng - Rp 10,000");
+        System.out.println("5. Mie Goreng - Rp 18,000");
+        System.out.println("===========================");
+        System.out.println("Silahkan pilih menu yang anda inginkan.");
+    }
+
+    public static void main(String[] args) {
+        Menu();
+    }
+}
